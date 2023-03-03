fe239b2448cfd85473dcf81b57167231603ca706
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-03-03 14:09:55 +0200

    setup express server

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("store api");
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const notFound = require("./middlewares/not-found");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+    res.send("<h1>Store API</h1><a href='/api/v1/products'>Products Route</a>");
+});
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        // Connect db
+        app.listen(port, console.log(`Server listening on port ${port} ...`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+start();
